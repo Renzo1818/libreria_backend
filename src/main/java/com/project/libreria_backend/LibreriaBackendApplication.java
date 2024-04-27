46ac4f4d42f1846110d2c0a33a64596c0c396468
RenzoZ <93232895+Renzo1818@users.noreply.github.com>
2024-04-26 21:19:54 -0500

    Se creó el proyecto libreria_backend

---
+++ src/main/java/com/project/libreria_backend/LibreriaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.libreria_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaBackendApplication.class, args);
+	}
+
+}
